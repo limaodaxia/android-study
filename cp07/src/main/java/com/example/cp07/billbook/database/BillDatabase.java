53a52db7b209a8fd36984b0e5612d8c3eb6a151b
limaodaxia <1161855028@qq.com>
2025-11-10 23:46:41 +0800

    (feat) 1.修改cp07和qwenalarm依赖的junit版本 2.新增模块08 3.billDatabase优化

--- cp07/src/main/java/com/example/cp07/billbook/database/BillDatabase.java
+++ cp07/src/main/java/com/example/cp07/billbook/database/BillDatabase.java
@@ -1,17 +1,14 @@
 package com.example.cp07.billbook.database;
 
-import android.util.Log;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-
 import com.example.cp07.billbook.MainApplication;
 import com.example.cp07.billbook.dao.BillDao;
 import com.example.cp07.billbook.entity.Bill;
 import com.example.cp07.billbook.entity.DateConverter;
-
 
 @Database(entities = {Bill.class}, version = 1, exportSchema = false)
 @TypeConverters({DateConverter.class})
@@ -21,7 +18,6 @@ public abstract class BillDatabase extends RoomDatabase{
         if (instance==null){
             synchronized (BillDatabase.class){
                 if (instance==null){
-                    Log.d("lxl",MainApplication.getAppContext()+"");
                     instance = Room.databaseBuilder(MainApplication.getAppContext(), BillDatabase.class,"bill_database")
                             .allowMainThreadQueries()
                             .build();
