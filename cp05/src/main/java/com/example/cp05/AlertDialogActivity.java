9b0ce4a85f5e69aed54e99b3da8382437c93bc4b
limaodaxia <1161855028@qq.com>
2025-12-25 22:56:59 +0800

    1. 添加一个弹出对话框

---
+++ cp05/src/main/java/com/example/cp05/AlertDialogActivity.java
@@ -0,0 +1,37 @@
+package com.example.cp05;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AlertDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alert_dialog);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");
+        builder.setMessage("确定要退出吗？");
+        builder.setPositiveButton("确定", (dialog, which) -> {
+            // 点击确定按钮后的操作
+        });
+        builder.setNegativeButton("取消", (dialog, which) -> {
+            // 点击取消按钮后的操作
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
