08a14b3d52adc8bcc4563a3f6cf5cc7324dd674e
limaodaxia <1161855028@qq.com>
2026-01-09 22:14:01 +0800

    1. 修改页面名

--- cp06/src/main/java/com/example/cp06/learningresolver/DBTestActivity.java
+++ cp06/src/main/java/com/example/cp06/learningresolver/DBTestActivity.java
@@ -27,7 +27,7 @@ public class DBTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_content_resolver);
+        setContentView(R.layout.activity_db_test);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
